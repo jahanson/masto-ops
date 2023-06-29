f9642a2c1a088ec50f95a09225c90b3976936864
Joseph Hanson <joe@veri.dev>
2023-06-29 10:39:21 -0500

    Updating infra for hetzner.

--- infrastructure/talos/valinor/apply.sh
+++ infrastructure/talos/valinor/apply.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n 10.2.1.10 -f ./clusterconfig/morgoth-galadriel.jahanson.tech.yaml
-talosctl apply-config -n 10.2.1.10 -f ./clusterconfig/morgoth-elrond.jahanson.tech.yaml
-talosctl apply-config -n 10.2.1.10 -f ./clusterconfig/morgoth-cirdan.jahanson.tech.yaml
+talosctl apply-config -n 10.2.1.10 -f ./clusterconfig/valinor-varda.hsn.dev.yaml
+talosctl apply-config -n 10.2.1.11 -f ./clusterconfig/valinor-manwe.hsn.dev.yaml
+talosctl apply-config -n 10.2.1.12 -f ./clusterconfig/valinor-aule.hsn.dev.yaml
