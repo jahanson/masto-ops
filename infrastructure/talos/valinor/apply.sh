9e0fe93a1c91396e79c29379b7f57b91443a46bf
Joseph Hanson <joe@veri.dev>
2023-08-04 13:01:21 -0500

    Adding new nodes.

--- infrastructure/talos/valinor/apply.sh
+++ infrastructure/talos/valinor/apply.sh
@@ -3,4 +3,7 @@
 # Deploy the configuration to the nodes
 talosctl apply-config -n 10.2.1.10 -f ./clusterconfig/valinor-varda.hsn.dev.yaml
 talosctl apply-config -n 10.2.1.11 -f ./clusterconfig/valinor-manwe.hsn.dev.yaml
-talosctl apply-config -n 10.2.1.12 -f ./clusterconfig/valinor-aule.hsn.dev.yaml
+talosctl apply-config -n 10.2.1.12 -f ./clusterconfig/valinor-nienna.hsn.dev.yaml
+talosctl apply-config -n 10.2.1.13 -f ./clusterconfig/valinor-orome.hsn.dev.yaml
+talosctl apply-config -n 10.2.1.14 -f ./clusterconfig/valinor-vaire.hsn.dev.yaml
+talosctl apply-config -n 10.2.1.15 -f ./clusterconfig/valinor-yavanna.hsn.dev.yaml
