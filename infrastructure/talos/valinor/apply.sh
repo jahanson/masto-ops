c30ff7e43e648fd2a3f6002181a7b4f1d82e29cb
Joseph Hanson <joe@veri.dev>
2023-08-10 13:07:00 -0500

    Updating old master names.

--- infrastructure/talos/valinor/apply.sh
+++ infrastructure/talos/valinor/apply.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n 10.2.1.10 -f ./clusterconfig/valinor-varda.hsn.dev.yaml
-talosctl apply-config -n 10.2.1.11 -f ./clusterconfig/valinor-manwe.hsn.dev.yaml
+talosctl apply-config -n 10.2.1.10 -f ./clusterconfig/valinor-eonwe.hsn.dev.yaml
+talosctl apply-config -n 10.2.1.11 -f ./clusterconfig/valinor-arlen.hsn.dev.yaml
 talosctl apply-config -n 10.2.1.12 -f ./clusterconfig/valinor-nienna.hsn.dev.yaml
 talosctl apply-config -n 10.2.1.13 -f ./clusterconfig/valinor-orome.hsn.dev.yaml
 talosctl apply-config -n 10.2.1.14 -f ./clusterconfig/valinor-vaire.hsn.dev.yaml
